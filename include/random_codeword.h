57b23f97f6005064ca436f6c16efa2a62e213445
qkhWsl <1917063458@qq.com>
2024-10-31 18:05:03 +0800

    2024/10/31

--- include/random_codeword.h
+++ include/random_codeword.h
@@ -1,7 +1,19 @@
 #pragma once
-#include <random>
 #include <Eigen/Dense>
+#include <random>
 
-void generate_codeword(Eigen::VectorXd& codeword) {
-    // 对于信息位随机生成0/1
+void generate_codeword(Eigen::VectorXi &codeword,
+                       Eigen::VectorXi &frozen_bits) {
+    // 初始化随机数生成器
+    std::random_device rd;                         // 用于生成随机种子
+    std::mt19937 generator(rd());                  // 随机数生成器
+    std::uniform_int_distribution<int> dist(0, 1); // 生成 0 或 1 的均匀分布
+
+    // 遍历 frozen_bits
+    for (int i = 0; i < frozen_bits.size(); ++i) {
+        if (frozen_bits[i] == 0) {         // 如果当前位置为信息位
+            codeword[i] = dist(generator); // 随机生成 0 或 1
+        }
+        // 如果 frozen_bits[i] 为 1，codeword[i] 保持原样
+    }
 }
